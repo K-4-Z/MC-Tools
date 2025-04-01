bd2bf0a230b9638873d37f825fba5a6cd1ca64a7
K-4-Z <fwastgt@outlook.de>
2025-04-01 18:41:18 +0200

    -

---
+++ src/main/java/k4z/mctools/K4ZMCToolsClient.java
@@ -0,0 +1,10 @@
+package k4z.mctools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class K4ZMCToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/k4z/mctools/K4ZMCToolsDataGenerator.java
@@ -0,0 +1,11 @@
+package k4z.mctools;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class K4ZMCToolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
